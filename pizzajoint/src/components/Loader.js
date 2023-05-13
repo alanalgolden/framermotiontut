28872522b98444ddc9201e1cbe1965427d0e4d56
Alana Golden <alanalgolden@gmail.com>
2023-05-13 15:41:00 -0400

    lesson 16

--- pizzajoint/src/components/Loader.js
+++ pizzajoint/src/components/Loader.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { motion } from "framer-motion";
+import { motion, useCycle } from "framer-motion";
 
 const loaderVariants = {
   animationOne: {
@@ -11,16 +11,26 @@ const loaderVariants = {
       y: { yoyo: Infinity, duration: 0.25, ease: "easeOut" }, //transition properties for x
     },
   },
+  animationTwo: {
+    y: [0, -40],
+    x: 0,
+    transition: {
+      y: { yoyo: Infinity, duration: 0.25, ease: "easeOut" },
+    },
+  },
 };
 
 const Loader = () => {
+  const [animation, cycleAnimation] = useCycle("animationOne", "animationTwo");
+
   return (
     <>
       <motion.div
         className="loader"
         variants={loaderVariants}
-        animate="animationOne"
-      ></motion.div>
+        animate={animation}
+      />
+      <div onClick={() => cycleAnimation()}>Cycle Loader</div>
     </>
   );
 };
